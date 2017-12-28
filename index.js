40bca14cbca8aa6893fe4d30eeba3ef4973c22ac
Johnnybar <johnnybareket@gmail.com>
2017-12-28 18:20:24 +0100

    playing some more with package json, maybe will fix heroku error

--- index.js
+++ index.js
@@ -221,9 +221,9 @@ io.on('connection', function(socket) {
 
 /////SERVER LISTEN
 
-var port = process.env.PORT || 5000;
+// var port = process.env.PORT || 5000;
 // app.listen(port, "0.0.0.0", function() {
 //     console.log("Listening on Port 3000");
 // });
 
-server.listen(port, "0.0.0.0");
+server.listen(process.env.PORT || 3000);
