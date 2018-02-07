349d75b0a02eca6ec2578cf7f6152a012b5c0790
Johnnybar <johnnybareket@gmail.com>
2018-02-07 11:55:49 +0100

    fixed hidden text in contact page

--- src/contact.js
+++ src/contact.js
@@ -29,7 +29,7 @@ componentDidMount() {
         <div>
             <div>
             <form className='contact-form-container' target="_blank"  action="mailto:johnnybareket@gmail.com" method="post" enctype="text/plain" >
-            <br/><div className='bg-white'>Are we missing your favorite cinema? Did we gain weight? Send us an email and we'll sort it out!</div><br/>
+            <br/><div className='paint-white-bg'>Are we missing your favorite cinema? Did we gain weight? Send us an email and we'll sort it out!</div><br/>
             Full Name: <input type="text" name="FullName" placeholder='Full Name'></input>
                 Email:<input type="text" name="Email" placeholder='Email Address'></input>
                     <textarea id= 'text-area-contact' placeholder='Let us know if you have any suggestions'></textarea>
